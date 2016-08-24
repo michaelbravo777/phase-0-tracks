56c7d5d499534ebaa53b40850fde6c85b979bec7
Michael Bravo <michael.bravo777@gmail.com>
2016-08-24 16:09:33 -0400

    adding allergy questionnaire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,4 @@
-puts "How many employees are you going to process?"
+puts "\r\nHow many employees are you going to process?"
 
 number_of_employees = gets.chomp
 
@@ -14,32 +14,37 @@ for i in 1..number_of_employees.to_i
 	employee_garlic = gets.chomp
 	puts "\r\nWould you like to enroll in the company’s health insurance?"
 	employee_insurance = gets.chomp
-	result = "\r\nResults inconclusive."
+	result = "Results inconclusive."
 	if (employee_age.to_i + employee_birth_year.to_i == Time.now.year) ||
 		(employee_age.to_i + employee_birth_year.to_i == Time.now.year - 1)
-		then if employee_garlic == "y" || employee_insurance == "y"
-			 	then result = "\r\nProbably not a vampire."
+		then if employee_garlic == "yes" || employee_insurance == "yes"
+			 	then result = "Probably not a vampire."
 			 end
-		elsif employee_garlic == "n" && employee_insurance == "n"
-			then result = "\r\nAlmost certainly a vampire."
-		elsif employee_garlic == "n" || employee_insurance == "n"
-			then result = "\r\nProbably a vampire."
+		elsif employee_garlic == "no" && employee_insurance == "no"
+			then result = "Almost certainly a vampire."
+		elsif employee_garlic == "no" || employee_insurance == "no"
+			then result = "Probably a vampire."
 	end
 	if employee_name == "Drake Cula" || employee_name == "Tu Fang"
-		then result = "\r\nDefinitely a vampire."
+		then result = "Definitely a vampire."
 	end
-end
 
-puts "\r\nEmployee #{employee_name}: #{result}"
+	puts "\r\nEmployee #{employee_name} - Questionnaire results: #{result}"
 
-allergies = ""
-until allergies == "done" || allergies == "sunlight" do
-   puts "\r\nDo you have an allergies? Type  \"done\" when finished"
-   allergies = gets.chomp
-end
+	allergies = ""
+	until allergies == "done" || allergies == "sunlight" do
+	   puts "\r\nDo you have an allergies? Type  \"done\" when finished"
+	   allergies = gets.chomp
+	end
 
-if allergies == "sunlight"
-	then puts "\r\nEmployee #{employee_name}: Probably a vampire."
+	result = "Results inconclusive."
+
+	if allergies == "sunlight"
+		then result = "Probably a vampire."
+	end
+
+	puts "\r\nEmployee #{employee_name} - Allergy results: #{result}"
+
 end
 
 puts "\r\nActually, never mind! What do these questions have to do with anything? Let's all be friends."
