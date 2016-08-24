179f62b0bab9cde4003fbbb7791ac9987f042378
Michael Bravo <michael.bravo777@gmail.com>
2016-08-24 16:17:54 -0400

    adding comments

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,16 +15,21 @@ for i in 1..number_of_employees.to_i
 	puts "\r\nWould you like to enroll in the company’s health insurance?"
 	employee_insurance = gets.chomp
 	result = "Results inconclusive."
+	#check if employee got their age right.
 	if (employee_age.to_i + employee_birth_year.to_i == Time.now.year) ||
 		(employee_age.to_i + employee_birth_year.to_i == Time.now.year - 1)
+		#check if employee got their age right, and is willing to eat garlic bread or sign up for insurance.
 		then if employee_garlic == "yes" || employee_insurance == "yes"
 			 	then result = "Probably not a vampire."
 			 end
+		#check if employee got their age wrong, hates garlic bread, and doesn’t want insurance
 		elsif employee_garlic == "no" && employee_insurance == "no"
 			then result = "Almost certainly a vampire."
+		#check if employee got their age wrong, and hates garlic bread or waives insurance
 		elsif employee_garlic == "no" || employee_insurance == "no"
 			then result = "Probably a vampire."
 	end
+	#check for vamire like names
 	if employee_name == "Drake Cula" || employee_name == "Tu Fang"
 		then result = "Definitely a vampire."
 	end
