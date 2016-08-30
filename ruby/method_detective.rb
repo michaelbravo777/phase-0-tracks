d0c153bbc05639e6e4c804417091251bb88caaa4
Michael Bravo <michael.bravo777@gmail.com>
2016-08-30 13:29:57 -0400

    Adding answers

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+p "iNvEsTiGaTiOn".swapcase
+p "InVeStIgAtIoN"
 
-# "zom".<???>
-# => “zoom”
+p "zom".insert(2, "o")
+p "zoom"
 
-# "enhance".<???>
-# => "    enhance    "
+p "enhance".center(15) 
+p "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+p "Stop! You're under arrest!".upcase
+p "STOP! YOU'RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+p "the usual".concat(" suspects")
+p "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+p " suspects".sub(' s', 'the usual s') 
+p "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+p "The case of the disappearing last letter".chop
+p "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+p "The mystery of the missing first letter".reverse.chop.reverse
+p "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+p "Elementary,    my   dear        Watson!".squeeze(" ")
+p "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+p "z".bytes
+p 122 
+p "(What is the significance of the number 122 in relation to the character z?)"
+p "122 is the ASCII number for the letter z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+p "How many times does the letter 'a' appear in this string?".count("a")
+p 4
