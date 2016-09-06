f5de40a7d64c6e5698a25f12bc229269829a6b8f
Michael Bravo <michael.bravo777@gmail.com>
2016-09-06 11:47:01 -0400

    adding comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Mexican Bulldog
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: if input is bacon
+# Tail: no
+# Legs: 4
+# Name: varies
+# Color: lime green
+
+# BEHAVIOR -----------------------
+# Bites
+# Runs
+# Sleeps
+
