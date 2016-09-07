fafc152cb9fa323dd7cbc5270fd578fbc5c875b8
Michael Bravo <michael.bravo777@gmail.com>
2016-09-07 10:04:50 -0400

    no changes

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,7 +1,7 @@
 class Puppy
 
   def Puppy.initialize()
-  	puts "Initializing new puppy instance ..."
+    puts "Initializing new puppy instance ..."
   end
 
   def fetch(toy)
@@ -10,21 +10,21 @@ class Puppy
   end
 
   def speak(num_of_woofs)
-  	num_of_woofs.times {puts "Woof!"}
+    num_of_woofs.times {puts "Woof!"}
   end
 
   def roll_over()
-  	puts "*Rolls Over*"
+    puts "*Rolls Over*"
   end
 
   def dog_years(num_of_years)
-  	dog_years = num_of_years * 7
-  	puts "#{num_of_years} is #{dog_years} in dog years"
+    dog_years = num_of_years * 7
+    puts "#{num_of_years} is #{dog_years} in dog years"
   end
 
   def eat(num_of_times)
-  	ounces = num_of_times * 3
-  	puts "Give him #{ounces} ounces every meal"
+    ounces = num_of_times * 3
+    puts "Give him #{ounces} ounces every meal"
   end
 
 end
@@ -32,16 +32,16 @@ end
 class Kittens
 
   def Kittens.initialize()
-  	puts "Initializing new Kitten instance"
+    puts "Initializing new Kitten instance"
   end
 
   def fetch(toy)
-  	puts "Here is your new toy, #{toy}"
+    puts "Here is your new toy, #{toy}"
   end
 
   def sleep(hours_per_day)
-  	hours_per_week = hours_per_day * 7
-  	puts "#{hours_per_week} is how many hours your cat will sleep per week"
+    hours_per_week = hours_per_day * 7
+    puts "#{hours_per_week} is how many hours your cat will sleep per week"
   end
 
 end
@@ -57,14 +57,14 @@ Puppy.initialize()
 cat_array = []
 
 50.times do
-	puts "Give us a cat name"
-	cat_name = gets.chomp
-	cat_name = Kittens.new
-	cat_array.push(cat_name)
+  puts "Give us a cat name"
+  cat_name = gets.chomp
+  cat_name = Kittens.new
+  cat_array.push(cat_name)
 end
 
 cat_array.each do |kitten|
-	puts kitten.fetch("yarn")
-	puts kitten.sleep(3)
-	puts Kittens.initialize()
+  puts kitten.fetch("yarn")
+  puts kitten.sleep(3)
+  puts Kittens.initialize()
 end
