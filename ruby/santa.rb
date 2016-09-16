7611e6f964fc0e9911cf786192fbd7ed7dcc6cc5
Michael Bravo <michael.bravo777@gmail.com>
2016-09-16 15:15:25 -0400

    forgot Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,7 +3,7 @@ class Santa
   attr_accessor :gender, :ethnicity, :age
 
   def initialize(gender, ethnicity)
-    puts "Initializing Santa instance ..."
+    puts "\n\nInitializing Santa instance ..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -18,7 +18,36 @@ class Santa
     puts "That was a good #{type_of_cookie}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(mad_at)
+    # p @reindeer_ranking
+    @reindeer_ranking << @reindeer_ranking.delete_at(@reindeer_ranking.index(mad_at))
+    # p @reindeer_ranking
+  end
+
+  def gender
+    @gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
 end
+
+santa = Santa.new("female", "Japanese-African")
+santa.get_mad_at("Prancer")
+santa.celebrate_birthday
+puts santa.age
+puts santa.ethnicity
+santa.gender = "N/A"
+puts santa.gender
 
 #santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
@@ -26,5 +55,5 @@ example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer n
 20.times do
   santa = Santa.new(example_genders.sample, example_ethnicities.sample)
   santa.age = 1 + rand(140)
-  puts "Gender: #{santa.gender} Ethnicity: #{santa.ethnicity} Age: #{santa.age}"
+  puts "\nGender: #{santa.gender} Ethnicity: #{santa.ethnicity} Age: #{santa.age}"
 end
