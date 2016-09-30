f9e4e45d52e4fb15640eb09740cd8ef5b154a7ff
Michael Bravo <michael.bravo777@gmail.com>
2016-09-30 15:34:15 -0400

    9.5 Mandatory Pairing: Templating in Sinatra

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,7 +1,9 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
+# this is to link the css sort of
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
@@ -9,9 +11,11 @@ db.results_as_hash = true
 
 # show students on the home page
 get '/' do
+  # puts data into our template:
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
+# erb :home accesses html template (erb is a method)
 
 get '/students/new' do
   erb :new_student
@@ -22,6 +26,30 @@ end
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+  # inserts student into database and redirects to home page
+end
+# All 3 methods of sending data to the server end up in params.
+# add static resources
+get '/campus' do
+  erb :campus
 end
 
-# add static resources
+get '/campusresults' do
+  @campus = db.execute("SELECT * FROM students WHERE campus=(?)", [params['campus']])
+  erb :campusresults
+end
+
+get '/campus/new' do
+  erb :new_campus
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+  redirect '/campus_list'
+end
+
+get '/campus_list' do
+  # puts data into our template:
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :campus_list
+end
